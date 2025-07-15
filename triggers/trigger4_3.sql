2fd3f41ff54fe1ffb0ea003158ec2b476a73b430
Luis Rey Lara <luisreylara@hotmail.com>
2025-07-15 11:03:14 -0600

    Update trigger4_3.sql

--- triggers/trigger4_3.sql
+++ triggers/trigger4_3.sql
@@ -3,3 +3,7 @@ insert into ventas (codigo, costo) values (222,22.22);
 insert into ventas (codigo, costo) values (333,33.00);
 
 select * from ventas;
+
+
+delete from ventas where codigo = 111;
+delete from ventas where codigo = 333;
