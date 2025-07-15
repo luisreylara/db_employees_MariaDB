7772dca17bf5cbbc203517e20ebea103c93d8efe
Luis Rey Lara <luisreylara@hotmail.com>
2025-07-15 11:00:03 -0600

    Create trigger4_3.sql

---
+++ triggers/trigger4_3.sql
@@ -0,0 +1,5 @@
+insert into ventas (codigo, costo) values (111,12.12);
+insert into ventas (codigo, costo) values (222,22.22);
+insert into ventas (codigo, costo) values (333,33.00);
+
+select * from ventas;
