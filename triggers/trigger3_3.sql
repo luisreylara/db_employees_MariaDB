e389c5127a341b9e23b93482cc79165dbe6f0bf9
Luis Rey Lara <luisreylara@hotmail.com>
2025-07-15 10:58:12 -0600

    Update trigger3_3.sql

--- triggers/trigger3_3.sql
+++ triggers/trigger3_3.sql
@@ -1,3 +1,5 @@
 insert into alumnos (nombre,edad) values ('luis',-1);
 insert into alumnos (nombre,edad) values ('pablo',250);
 insert into alumnos (nombre,edad) values ('pedro',55);
+
+select * from alumnos;
